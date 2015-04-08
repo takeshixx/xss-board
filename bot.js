a7dcc40c7c9b34c6a93581014af172c2d92794c3
takeshix <takeshix@adversec.com>
2015-04-08 14:06:23 +0200

    Zombie.js 2.1.1 compatible code added

--- bot.js
+++ bot.js
@@ -1,12 +1,16 @@
-// author: takeshix@adversec.com
-var zombie = require("zombie");
-var bot = new zombie();
-bot.cookies("localhost","/index.php").set("PHPSESSID", "mag1c_c00k1e");
-bot.visit("http://localhost:83/", {debug: true}, function(err,browser){
-	browser.
-		fill("user", "admin").
-		fill("comment", "hilarious!").
-		pressButton("Post it!", function(){})
-});
+var Browser = require("zombie");
+var assert  = require('assert');
+Browser.localhost('localhost', 80);
+var browser = Browser.create();
 
+browser.setCookie({name: "PHPSESSID", value: "mag1c_c00k1e"});
+browser.visit("/index.php", {debug: true}, function(error){
+    assert.ifError(error);
+        browser.
+                fill("user", "admin").
+                fill("comment", "hilarious!").
+                pressButton("Post it!", function(error){
+            assert.ifError(error);
+        })
+});
 
